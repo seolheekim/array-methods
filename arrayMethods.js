0a4c11f2c62b7d2022f3fea5f81a959ca7902506
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-13 19:44:32 -1000

    pop

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,11 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+var nameArray = ["Kim", "Seolhee"];
+
+genericNumberArray.push(6, 7, 8, 9, 10);
+
+colors.pop("Charlie");
+
+var popResult.pop();
