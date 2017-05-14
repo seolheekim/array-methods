dc34e4526146f687163994a990621002ba6c09ef
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-13 19:49:16 -1000

    reverse

--- arrayMethods.js
+++ arrayMethods.js
@@ -17,10 +17,14 @@ var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
 
-var nameArray = ["Kim", "Seolhee"];
+var nameArray = [];
+nameArray.push("Kim", "Seolhee")
 
 genericNumberArray.push(6, 7, 8, 9, 10);
 
-colors.pop("Charlie");
+var popResult = colors.pop("Charlie");
 
-var popResult.pop();
+scrambledWords.reverse();
+
+phoneNumber.reverse();
+
