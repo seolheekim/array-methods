ad56d6bb11d807068ed963a486d3396df88e32b2
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-14 20:39:08 -1000

    unshift()

--- arrayMethods.js
+++ arrayMethods.js
@@ -41,3 +41,5 @@ gemBox.splice(5, 7, 'Sapphire');
 upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
 
 brownOnly.splice(1, 5, "brown", "brown", "brown");
+
+var orderedValuesNewLength = orderedValues.unshift(1, 2, 3);
